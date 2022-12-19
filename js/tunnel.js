8cea3f52c54183b23aaba7326942d5923c70fa11
renevds <rene.v.d.s.9@gmail.com>
2022-12-19 16:35:12 +0100

    Fix site

--- js/tunnel.js
+++ js/tunnel.js
@@ -18,19 +18,30 @@ let cHeight;
 let cWidth;
 let maxVerDelta;
 let maxHorDelta;
-let lastRender = 0;
+let lastRender = -tunnelFrameTime - 1;
+let scale = window.devicePixelRatio;
+let prevWidth;
 
 // Handle canvas resize and initial canvas setup
 function resizeCanvas() {
-  tunnelCanvas.width = part1.offsetWidth;
-  tunnelCanvas.height = part1.offsetHeight;
-  tunnelCtx.resetTransform();
-  tunnelCtx.translate(tunnelCanvas.width / 2, tunnelCanvas.height / 2);
-  tunnelCtx.lineWidth = 1;
-  cHeight = tunnelCanvas.height;
-  cWidth = tunnelCanvas.width;
-  maxVerDelta = cHeight / 2;
-  maxHorDelta = cWidth / 2;
+  if(prevWidth !== window.innerWidth) {
+    prevWidth = window.innerWidth;
+    part1.style.height = `${window.innerHeight}px`;
+    tunnelCanvas.style.width = `${part1.offsetWidth}px`;
+    tunnelCanvas.style.height = `${part1.offsetHeight}px`;
+    console.log(scale);
+    tunnelCanvas.width = part1.offsetWidth * scale;
+    tunnelCanvas.height = part1.offsetHeight * scale;
+    tunnelCtx.resetTransform();
+    tunnelCtx.translate(tunnelCanvas.width / 2, tunnelCanvas.height / 2);
+    tunnelCtx.scale(scale, scale);
+    tunnelCtx.lineWidth = 1;
+    cHeight = tunnelCanvas.height / scale;
+    cWidth = tunnelCanvas.width / scale;
+    maxVerDelta = cHeight / 2;
+    maxHorDelta = cWidth / 2;
+    drawOneFrame();
+  }
 }
 
 window.addEventListener("resize", resizeCanvas, false);
@@ -41,32 +52,20 @@ window.addEventListener("mousemove", e => {
   const rect = tunnelCanvas.getBoundingClientRect();
   const x = (e.clientX - rect.left);
   const y = (e.clientY - rect.top);
-  horDelta = (x / tunnelCanvas.width) * 2 - 1;
-  verDelta = (y / tunnelCanvas.height) * 2 - 1;
-});
-
-// Handle touch movement
-window.addEventListener("touchmove ", e => {
-  const rect = tunnelCanvas.getBoundingClientRect();
-  const x = (e.clientX - rect.left);
-  const y = (e.clientY - rect.top);
-  horDelta = (x / tunnelCanvas.width) * 2 - 1;
-  verDelta = (y / tunnelCanvas.height) * 2 - 1;
-
+  horDelta = (x / cWidth) * 2 - 1;
+  verDelta = (y / cHeight) * 2 - 1;
 });
 
 // Handle scroll
 document.addEventListener("scroll", function (e) {
-  if(window.scrollY> 0){
-    const y = -window.scrollY * fadeScrollMultiplier + tunnelCanvas.offsetHeight;
-    fade = Math.max(0, y / tunnelCanvas.height);
-    render = fade > 0;
-    let alpha = fade * 256;
-    let color = `rgb(${alpha},${alpha},${alpha})`;
-    document.getElementById("scroll").style.borderColor = color;
-    document.getElementById("scroll_dot").style.backgroundColor = color;
-    document.getElementById("part1_scroll").style.color = color;
-  }
+  const y = -window.scrollY * fadeScrollMultiplier + cHeight;
+  fade = Math.max(0, y / cHeight);
+  render = fade > 0;
+  let alpha = fade * 256;
+  let color = `rgb(${alpha},${alpha},${alpha})`;
+  document.getElementById("scroll").style.borderColor = color;
+  document.getElementById("scroll_dot").style.backgroundColor = color;
+  document.getElementById("part1_scroll").style.color = color;
 });
 
 // Functions for drawing the tunnel
@@ -89,55 +88,59 @@ function angularToCartesian(r, j) {
 
 function drawTunnel(time) {
   if (time - lastRender > tunnelFrameTime) {
-    lastRender = Math.floor(time/tunnelFrameTime)*tunnelFrameTime;
+    lastRender = Math.floor(time / tunnelFrameTime) * tunnelFrameTime;
     tunnelFrame++;
-    if(render) {
-      let inner = [];
-      let current = [];
-      let radius = (Math.sqrt((cHeight + maxHorDelta) ** 2 + (cWidth + maxVerDelta) ** 2) / 2) * (partDelta ** ((tunnelFrame % framesPerLevel) / framesPerLevel));
-
-      for (let i = tunnelDepth - 1; i >= 0; i--) {
-        for (let j = 0; j < tunnelParts; j++) {
-          const r = radius / (partDelta ** i);
-          let coords = angularToCartesian(r, j);
-          const edgeRad = i - (tunnelFrame % framesPerLevel) / framesPerLevel;
-          coords[0] += maxHorDelta * Math.sin(2 * Math.PI * edgeRad / tunnelParts * horDelta);
-          coords[1] += maxVerDelta * Math.sin(2 * Math.PI * edgeRad / tunnelParts * verDelta);
-          current.push(coords);
-        }
-        if (i < tunnelDepth - 1) {
-          tunnelCtx.beginPath();
-          for (let j = 0; j < inner.length; j++) {
-            const jn = (j + 1) % inner.length;
-            tunnelCtx.moveTo(inner[j][0], inner[j][1]);
-            tunnelCtx.lineTo(current[j][0], current[j][1]);
-            tunnelCtx.lineTo(current[jn][0], current[jn][1]);
-            tunnelCtx.lineTo(inner[jn][0], inner[jn][1]);
-            tunnelCtx.closePath();
-          }
-          tunnelCtx.fillStyle = "black";
-          tunnelCtx.fill();
-          let alpha = fade * ((tunnelDepth - i) / tunnelDepth) * fadeMultiplier * 256;
-          tunnelCtx.strokeStyle = `rgb(${alpha},${alpha},${alpha})`;
-          tunnelCtx.stroke();
-        } else {
-          tunnelCtx.beginPath();
-          tunnelCtx.moveTo(current[0][0], current[0][1]);
-          current.forEach(coord => {
-            tunnelCtx.lineTo(coord[0], coord[1]);
-          });
-          tunnelCtx.fillStyle = "black";
-          tunnelCtx.fill();
-        }
-        inner = current;
-        current = [];
-      }
+    if (render) {
+      drawOneFrame();
     }
   }
-  if(!render){
+  if (!render) {
     clearCanvas();
   }
   window.requestAnimationFrame(drawTunnel);
+}
+
+function drawOneFrame() {
+  let inner = [];
+  let current = [];
+  let radius = (Math.sqrt((cHeight + maxHorDelta) ** 2 + (cWidth + maxVerDelta) ** 2) / 2) * (partDelta ** ((tunnelFrame % framesPerLevel) / framesPerLevel));
+
+  for (let i = tunnelDepth - 1; i >= 0; i--) {
+    for (let j = 0; j < tunnelParts; j++) {
+      const r = radius / (partDelta ** i);
+      let coords = angularToCartesian(r, j);
+      const edgeRad = i - (tunnelFrame % framesPerLevel) / framesPerLevel;
+      coords[0] += maxHorDelta * Math.sin(2 * Math.PI * edgeRad / tunnelParts * horDelta);
+      coords[1] += maxVerDelta * Math.sin(2 * Math.PI * edgeRad / tunnelParts * verDelta);
+      current.push(coords);
+    }
+    if (i < tunnelDepth - 1) {
+      tunnelCtx.beginPath();
+      for (let j = 0; j < inner.length; j++) {
+        const jn = (j + 1) % inner.length;
+        tunnelCtx.moveTo(inner[j][0], inner[j][1]);
+        tunnelCtx.lineTo(current[j][0], current[j][1]);
+        tunnelCtx.lineTo(current[jn][0], current[jn][1]);
+        tunnelCtx.lineTo(inner[jn][0], inner[jn][1]);
+        tunnelCtx.closePath();
+      }
+      tunnelCtx.fillStyle = "black";
+      tunnelCtx.fill();
+      let alpha = fade * ((tunnelDepth - i) / tunnelDepth) * fadeMultiplier * 256;
+      tunnelCtx.strokeStyle = `rgb(${alpha},${alpha},${alpha})`;
+      tunnelCtx.stroke();
+    } else {
+      tunnelCtx.beginPath();
+      tunnelCtx.moveTo(current[0][0], current[0][1]);
+      current.forEach(coord => {
+        tunnelCtx.lineTo(coord[0], coord[1]);
+      });
+      tunnelCtx.fillStyle = "black";
+      tunnelCtx.fill();
+    }
+    inner = current;
+    current = [];
+  }
 }
 
 drawTunnel(0);
