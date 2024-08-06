808aa81b757b8fde3dc9e1c2e28620748882e7b0
renevds <rene.v.d.s.9@gmail.com>
2024-08-06 16:53:01 +0200

    :sparkles: Improve v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#EA2626',
+      }
+    },
   },
   plugins: [],
 }
